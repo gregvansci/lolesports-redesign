a3246b693a6aa82cf56a2f9775317ed94f42f32b
gregvansci <gregvansci76@hotmail.com>
2023-03-16 16:39:32 -0700

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import "../app.css";
+	import Header from "../routes/Header.svelte";
+  </script>
+
+<div class="w-full h-full">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+	</footer>
+</div>
