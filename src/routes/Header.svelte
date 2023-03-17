daee04e19bf8423808c3b4f5479736fe97dab8aa
gregvansci <gregvansci76@hotmail.com>
2023-03-17 14:06:33 -0700

    Added dark mode toggle

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,19 +1,53 @@
-<script>
-	// get the black riot logo from the assets folder and save it as src
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
+	
 	import riotLogo from '$lib/images/riot-logo-black.png';
+	import riotLogoDark from '$lib/images/riot-logo-white.png';
 	import lolesportsLogo from '$lib/images/lolesports-logo-black.png';
 	import expandArrow from '$lib/images/expand-arrow.svg';
-	import lightMode from '$lib/images/light-mode.svg';
+	import lightModeIcon from '$lib/images/light-mode.svg';
+	import darkModeIcon from '$lib/images/dark-mode.svg';
 	import settings from '$lib/images/settings.svg';
 	import login from '$lib/images/login.svg';
+
+	let darkMode: boolean;
+
+	onMount(() => {
+		if (browser) {
+			if (localStorage.theme === "dark") {
+				darkMode = true;
+			} else {
+				darkMode = false;
+			}
+		}
+	});
+
+	function toggleDarkMode() {
+		if (darkMode) {
+			localStorage.theme = "light";
+			document.documentElement.classList.remove("dark");
+			darkMode = false;
+		} else {
+			localStorage.theme = "dark";
+			document.documentElement.classList.add("dark");
+			darkMode = true;
+		}
+	}
+
 </script>
 
-<header class="h-[60px] w-full bg-header-bg-light text-header-text-light border-b-[1px] border-header-tertiary-light shadow-sm">
+<header class="
+	h-[60px] w-full border-b-[1px] border-header-tertiary-light shadow-sm 
+	bg-header-bg-light dark:bg-header-bg-dark
+	text-header-text-light dark:text-header-text-dark  "
+>
 	<div class="max-w-[1400px] flex flex-row justify-between h-full w-full m-auto">
 		<div class="flex flex-row gap-4 h-full">
-			<img src={riotLogo} alt="Riot Games" class="h-[20px] m-auto" />
-			<div class="m-auto px-2 py-1 bg-header-secondary-light rounded-full flex flex-row hover:bg-header-tertiary-light cursor-pointer">
-				<img src={lolesportsLogo} alt="LoL Esports" class="h-[20px] m-auto pl-2 logo-filter" />
+			<img src={darkMode ? riotLogoDark : riotLogo} alt="Riot Games" class="h-[20px] m-auto" />
+			<div class="m-auto px-2 py-1 bg-header-secondary-light dark:bg-header-secondary-dark rounded-full flex flex-row hover:bg-header-tertiary-light dark:hover:bg-header-tertiary-dark cursor-pointer">
+				<img src={lolesportsLogo} alt="LoL Esports" class="h-[20px] m-auto pl-2 {darkMode ? "logo-filter-dark" : "logo-filter"}" />
 				<img src={expandArrow} alt="Expand" class="h-[20px] m-auto" />
 			</div>
 		</div>
@@ -34,11 +68,13 @@
 					</li>
 				</ul>
 			</nav>
-			<div class="h-[23px] m-auto border-l-[2px] border-header-tertiary-light"/>
+			<div class="h-[23px] m-auto border-l-[2px] border-header-tertiary-light dark:border-header-tertiary-dark"/>
 			<div class="flex flex-row gap-4">
-				<img src={lightMode} alt="Light Mode" class="h-[34px] m-auto svg-filter cursor-pointer px-2 py-[6px]" />
-				<img src={settings} alt="Settings" class="h-[34px] m-auto svg-filter cursor-pointer px-2 py-[6px]" />
-				<img src={login} alt="Login" class="h-[34px] m-auto svg-filter cursor-pointer px-2 py-[6px]" />
+				<button on:click={toggleDarkMode}>
+					<img src={lightModeIcon} alt="Light Mode" class="h-[34px] m-auto cursor-pointer px-2 py-[6px] {darkMode ? "svg-filter-dark" : "svg-filter"}" />
+				</button>
+				<img src={settings} alt="Settings" class="h-[34px] m-auto cursor-pointer px-2 py-[6px] {darkMode ? "svg-filter-dark" : "svg-filter"}" />
+				<img src={login} alt="Login" class="h-[34px] m-auto cursor-pointer px-2 py-[6px] {darkMode ? "svg-filter-dark" : "svg-filter"}" />
 			</div>
 		</div>
 	</div>
@@ -50,10 +86,19 @@
 	.logo-filter {
 		filter: invert(49%) sepia(10%) saturate(960%) hue-rotate(176deg) brightness(88%) contrast(86%);
 	}
+	.logo-filter-dark {
+		filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%);
+	}
 	.svg-filter {
 		filter: invert(49%) sepia(10%) saturate(960%) hue-rotate(176deg) brightness(88%) contrast(86%);
 	}
 	.svg-filter:hover {
+		filter: invert(58%) sepia(92%) saturate(3440%) hue-rotate(145deg) brightness(106%) contrast(101%);
+	}
+	.svg-filter-dark {
+		filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%);
+	}
+	.svg-filter-dark:hover {
 		filter: invert(58%) sepia(92%) saturate(3440%) hue-rotate(145deg) brightness(106%) contrast(101%);
 	}
 </style>
