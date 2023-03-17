1ce910f01ca95aa4d4ce691c48b3775c6d2dc5e5
gregvansci <gregvansci76@hotmail.com>
2023-03-16 21:28:32 -0700

    Updated fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      'sans': ['Segoe UI', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'],
+      'serif': ['Times New Roman', 'Times', 'serif'],
+      'title': ['Work Sans', 'Roboto', 'sans-serif'],
+      'body': ['Work Sans', 'Roboto', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
