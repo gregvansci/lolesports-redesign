593442b4e03aeea127c017c155989883e1a9a065
gregvansci <gregvansci76@hotmail.com>
2023-03-20 00:25:07 -0700

    Added stream embed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,15 @@
 </svelte:head>
 
 <section class="text-blue-gray-500 dark:text-white h-full flex flex-col justify-between pt-[60px]">
-	<div class="flex flex-row m-auto h-full pt-[32px] pb-[32px] gap-8">
+	<div class="flex flex-row m-auto h-full pt-[32px] pb-[32px]">
 		<div class="bg-gray-50 dark:bg-gray-800 h-full w-[1000px] transition ease-in-out duration-300">
-			video
+			<iframe
+				title="Target iframe page"
+				src="https://player.twitch.tv/?channel=lec&parent=streamernews.example.com&muted=true"
+				height="100%"
+				width="100%"
+				allowfullscreen>
+			</iframe>
 		</div>
 		<div class="bg-gray-50 dark:bg-gray-800 h-full w-[200px] transition ease-in-out duration-300">
 			live games
