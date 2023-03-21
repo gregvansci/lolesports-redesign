5d7b5af0ee10019547be87c79faff737061a5dda
gregvansci <gregvansci76@hotmail.com>
2023-03-20 21:42:30 -0700

    Added footer to home page

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,12 @@
+<footer class="w-full h-24 flex flex-row text-blue-gray-600 dark:text-blue-gray-400">
+    <div class="m-auto flex flex-row gap-4 mr-0">
+        <h2>made by <a class="text-highlight dark:text-highlight/80" href="https://www.github.com/gregvansci" target="_blank">gregvansci</a> (c) 2023</h2>
+        <div class="border-l-[1px] h-6 my-auto border-blue-gray-600 dark:border-blue-gray-400"/>
+    </div>
+    <div class="m-auto ml-4">
+        <h2 class="text-xs align-middle">
+            This website is not endorsed by Riot Games and does not reflect the views or opinions of Riot Games <br/>
+            or anyone officially involved in producing or managing League of Legends or LoLEsports. 
+        </h2>
+    </div>
+</footer>
