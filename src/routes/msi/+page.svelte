8c821f7bdd093de2f0b8defd0941b382aa870793
gregvansci <55711383+gregvansci@users.noreply.github.com>
2023-04-03 15:43:38 -0700

    Added MSI qualified teams layout

--- src/routes/msi/+page.svelte
+++ src/routes/msi/+page.svelte
@@ -7,13 +7,135 @@
     <img src={msiBg} class="h-full z-0 mx-auto absolute top-0 left-1/2 transform -translate-x-1/2" alt="BG">
     
     <div class="z-10 w-full h-full overflow-y-scroll">
-        <div class="z-20 w-[900px] rounded-xl mx-auto mt-[60vh] flex flex-col frosted-glass backdrop-filter backdrop-blur-2xl">
-            <div class="text-center pt-16 pb-6 h-[40vh] flex flex-col text-white">
-                <h1 class=" font-msi-title text-6xl tracking-wide mb-2">MSI 2023</h1>
-                <h2 class="font-msi-title text-5xl mb-10">DEFY</h2>
+        <div class="z-20 w-[900px] rounded-xl mx-auto mt-[60vh] py-16 mb-[20vh] text-white flex flex-col gap-8 frosted-glass backdrop-filter backdrop-blur-2xl">
+            <div class="text-center pb-6 h-[40vh] flex flex-col">
+                <h1 class=" font-msi-title text-6xl tracking-wide mb-2 bg-msi-text px-4 mx-auto">MSI 2023</h1>
+                <h2 class="font-msi-title text-5xl mb-10 bg-msi-text px-4 mx-auto">DEFY</h2>
                 <h2 class="font-msi-body font-bold text-4xl mb-10">LONDON MAY 2-21</h2>
                 <h2 class="font-msi-body font-semibold">SPOILERS BELOW</h2>
             </div>
+            <h1 class="text-center font-msi-body text-4xl font-semibold">QUALIFIED TEAMS</h1>
+            <div class="text-center flex flex-col h-[400px]">
+                <h1 class="font-msi-body text-3xl font-semibold mb-10">MAJOR</h1>
+                <div class="flex flex-row justify-center h-full w-full">
+                    <div class="flex flex-col justify-between pr-6 font-semibold font-msi-body text-xl">
+                        <h2 class="m-auto underline">SEED</h2>
+                        <h2 class="m-auto">1</h2>
+                        <h2 class="m-auto">2</h2>
+                    </div>
+                    <div class="bg-msi-text flex flex-row w-[600px] h-full border-[1px] border-white">
+                        <div class="flex flex-col w-1/4">
+                            <div class="w-full h-1/3 flex">
+                                <img class="h-[75px] m-auto" src="https://am-a.akamaihd.net/image?resize=120:&f=http%3A%2F%2Fstatic.lolesports.com%2Fleagues%2Flck-color-on-black.png" alt="LCK" />
+                            </div>
+                            <div class="w-full h-1/3 flex">
+                                <div class="m-auto">
+                                </div>
+                            </div>
+                            <div class="w-full h-1/3 flex">
+                                <div class="m-auto">
+                                    <img src="https://am-a.akamaihd.net/image?resize=72:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1631819523085_t1-2021-worlds.png" alt="team1" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col w-1/4">
+                            <div class="w-full h-1/3 flex border-l-[1px] border-white">
+                                <img class="h-[75px] m-auto" src="https://am-a.akamaihd.net/image?resize=120:&f=http%3A%2F%2Fstatic.lolesports.com%2Fleagues%2F1592516115322_LPL-01-FullonDark.png" alt="LPL" />
+                            </div>
+                            <div class="w-full h-1/3 flex border-l-[1px] border-white">
+                                <div class="m-auto">
+                                </div>
+                            </div>
+                            <div class="w-full h-1/3 flex border-l-[2px] border-t-[2px] border-msi-bg">
+                                <div class="m-auto">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex flex-col w-1/4 border-l-[1px] border-white">
+                            <div class="w-full h-1/3 flex">
+                                <img class="h-[75px] m-auto" src="https://am-a.akamaihd.net/image?resize=120:&f=http%3A%2F%2Fstatic.lolesports.com%2Fleagues%2F1592516184297_LEC-01-FullonDark.png" alt="LEC" />
+                            </div>
+                            <div class="w-full h-1/3 flex">
+                                <div class="m-auto">
+                                </div>
+                            </div>
+                            <div class="w-full h-1/3 flex border-t-[2px] border-msi-bg">
+                                <img class="h-[75px] m-auto" src="https://am-a.akamaihd.net/image?resize=140:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2FG2-FullonDark.png" alt="team1" />
+                            </div>
+                        </div>
+                        <div class="flex flex-col w-1/4 border-l-[1px] border-white">
+                            <div class="w-full h-1/3 flex">
+                                <img class="h-[75px] m-auto" src="https://am-a.akamaihd.net/image?resize=120:&f=http%3A%2F%2Fstatic.lolesports.com%2Fleagues%2FLCSNew-01-FullonDark.png" alt="LCS" />
+                            </div>
+                            <div class="w-full h-1/3 flex">
+                                <div class="m-auto">
+                                </div>
+                            </div>
+                            <div class="w-full h-1/3 flex border-t-[2px] border-msi-bg">
+                                <img class="h-[75px] m-auto" src="https://am-a.akamaihd.net/image?resize=140:&f=http%3A%2F%2Fstatic.lolesports.com%2Fteams%2F1631820065346_cloud9-2021-worlds.png" alt="team1" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="flex flex-col justify-between pl-6 font-semibold font-msi-body text-lg">
+                        <h2 class="m-auto underline">STAGE</h2>
+                        <h2 class="m-auto">BRACKET</h2>
+                        <h2 class="m-auto">PLAYIN</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center flex flex-col pt-2 mb-24">
+                <h1 class="font-msi-body text-3xl font-semibold pb-4">MINOR</h1>
+                <h2 class="font-msi-body text-xl font-semibold pb-10">TO PLAYIN STAGE</h2>
+                <div class="bg-msi-text w-[700px] h-[275px] flex flex-row m-auto border-[1px] border-white font-msi-body text-lg font-semibold">
+                    <div class="w-1/5 flex flex-col">
+                        <div class="h-1/2 w-full flex flex-col justify-between">
+                            <img class="m-auto mb-0 h-[75px]" src="https://am-a.akamaihd.net/image?resize=120:&f=http%3A%2F%2Fstatic.lolesports.com%2Fleagues%2F1592515942679_PCS-01-FullonDark.png" alt="PCS" />
+                            <h2 class="m-auto ">PCS</h2>
+                        </div>
+                        <div class="h-1/2 w-full flex">
+                            
+                        </div>
+                    </div>
+                    <div class="w-1/5 flex flex-col">
+                        <div class="h-1/2 w-full flex flex-col justify-between">
+                            <img class="m-auto mb-0 h-[75px]" src="https://am-a.akamaihd.net/image?resize=120:&f=http%3A%2F%2Fstatic.lolesports.com%2Fleagues%2F1677578094811_VCS_SYMBOL_TITANIUM_RGB.png" alt="VCS" />
+                            <h2 class="m-auto">VCS</h2>
+                        </div>
+                        <div class="h-1/2 w-full flex">
+                            
+                        </div>
+                    </div>
+                    <div class="w-1/5 flex flex-col">
+                        <div class="h-1/2 w-full flex flex-col justify-between">
+                            <img class="m-auto mb-0 h-[75px]" src="https://am-a.akamaihd.net/image?resize=120:&f=http%3A%2F%2Fstatic.lolesports.com%2Fleagues%2F1592516354053_LJL-01-FullonDark.png" alt="LJL" />
+                            <h2 class="m-auto">LJL</h2>
+                        </div>
+                        <div class="h-1/2 w-full flex">
+                            
+                        </div>
+                    </div>
+                    <div class="w-1/5 flex flex-col">
+                        <div class="h-1/2 w-full flex flex-col justify-between">
+                            <img class="m-auto mb-0 h-[75px]" src="https://am-a.akamaihd.net/image?resize=120:&f=http%3A%2F%2Fstatic.lolesports.com%2Fleagues%2Fcblol-logo-symbol-offwhite.png" alt="CBLOL" />
+                            <h2 class="m-auto">CBLOL</h2>
+                        </div>
+                        <div class="h-1/2 w-full flex">
+                            
+                        </div>
+                    </div>
+                    <div class="w-1/5 flex flex-col">
+                        <div class="h-1/2 w-full flex flex-col justify-between">
+                            <img class="m-auto mb-0 h-[75px]" src="https://am-a.akamaihd.net/image?resize=120:&f=http%3A%2F%2Fstatic.lolesports.com%2Fleagues%2F1592516315279_LLA-01-FullonDark.png" alt="LLA" />
+                            <h2 class="m-auto">LLA</h2>
+                        </div>
+                        <div class="h-1/2 w-full flex">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h1 class="text-center font-msi-body text-4xl font-semibold">FORMAT</h1>
+            <h2 class="font-msi-body text-xl font-semibold pb-10 text-center">COMING SOON</h2>
         </div>
     </div>
 </div>
